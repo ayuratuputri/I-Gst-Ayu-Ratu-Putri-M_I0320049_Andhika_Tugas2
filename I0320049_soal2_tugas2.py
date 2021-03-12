23260c827ec0c3671e7ed0b466c4532d576bd8f7
I Gusti Ayu Ratu Putri Maharani <ayuratuputrimaha@gmail.com>
2021-03-12 12:30:32 +0800

    pengerjaan soal 2 tugas 2

---
+++ I0320049_soal2_tugas2.py
@@ -0,0 +1,44 @@
+nama_lengkap ="I Gusti Ayu Ratu Putri Maharani"
+tempat_lahir ="Denpasar"
+tanggal_lahir ="7"
+bulan_lahir ="Mei"
+tahun_lahir ="2002"
+umur = 19
+tinggi = 168
+berat_badan = float(input("Masukkan berat badan: "))
+alamat_tinggal ="Jalan Astasura I Gang Mertasari No 88X Denpasar"
+lingkungan_sekitar ="Daerah tempat tinggal saya masih banyak persawahan karena saya tinggal di pinggiran kota. Namun, jarak rumah saya ke pusat kota tidaklah jauh."
+ukuran_sepatu = 39
+hobi ="Berenang dan membuat kerajinan tangan"
+makanan_kesukaan ="pecel lele"
+angkatan = 2020
+jumlah_saudara = 2
+jurusan_kuliah ="Teknik Industri"
+universitas ="Universitas Sebelas Maret"
+cita_cita ="pengusaha atau seorang manager"
+kesibukan ="mengikuti perkuliahan daring semester 2, mengikuti organisasi HMTI 2021 di bidang Danus"
+alasan_memilih_teknik_industri ="karena di dalam program studi teknik industri mempelajari banyak hal, tidak mempelajari keteknikan saja."
+deskripsi_singkat ="saya adalah orang yang cenderung tertutup ketika bertemu dengan teman baru karena ia merasa asing."
+hobi_lain ="belajar matematika seperti contohnya menghitung luas persegi panjang menggunakan pycharm"
+print("Nama saya adalah",nama_lengkap)
+print("lahir di", tempat_lahir)
+print("pada tanggal", tanggal_lahir)
+print("tahun", tahun_lahir)
+print("umur saya saat ini", umur)
+print("tinggi saya", tinggi)
+print("berat badan saya adalah", berat_badan)
+print("saya tinggal di", alamat_tinggal)
+print(lingkungan_sekitar)
+print("ukuran sepatu saya adalah", ukuran_sepatu)
+print("saya memiliki hobi", hobi)
+print("selain itu saya memiliki hobi lain yaitu", hobi_lain)
+print("makanan kesukaan saya adalah", makanan_kesukaan)
+print("saya angkatan", angkatan)
+print("saya memiliki saudara berjumlah", jumlah_saudara)
+print("saya kuliah di jurusan", jurusan_kuliah)
+print(universitas)
+print("saya bercita-cita menjadi", cita_cita)
+print("kesibukan saya saat ini adalah", kesibukan)
+print("alasan saya memilih teknik industri", alasan_memilih_teknik_industri)
+print("penjelasan singkat tentang saya adalah", deskripsi_singkat)
+
